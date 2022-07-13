5cc37b527b004e2804a86f87c0aae7f43ed30490
Sam Symons <sam@samsymons.com>
2022-07-12 17:06:36 -0700

    Remove deliberate failures.

--- Sources/TrackerRadarKit/ArrayExtension.swift
+++ Sources/TrackerRadarKit/ArrayExtension.swift
@@ -21,8 +21,6 @@ import Foundation
 extension Array where Element: Hashable {
 
     func removeDuplicates() -> [Element] {
-        deliberateCompilationFailure()
-        
         var existingElements = Set<Element>()
         return self.filter { existingElements.insert($0).inserted }
     }

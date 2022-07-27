d422ab9f38625bc2a43ed7b05aa45ccbbbd81fd3
Dominik Kapusta <dkapusta@duckduckgo.com>
2022-07-27 08:19:20 +0200

    Break tests

--- Tests/TrackerRadarKitTests/ArrayExtensionTests.swift
+++ Tests/TrackerRadarKitTests/ArrayExtensionTests.swift
@@ -32,7 +32,7 @@ class ArrayExtensionTests: XCTestCase {
         let array = Array(1...100) + Array(1...100)
         let deduplicated = array.removeDuplicates()
 
-        XCTAssertEqual(array.count, 200)
+        XCTAssertEqual(array.count, 202)
         XCTAssertEqual(deduplicated.count, 100)
         XCTAssertEqual(deduplicated, Array(1...100))
     }
